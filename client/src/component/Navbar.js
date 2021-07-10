444270e30e8a66ca33ba58807305daf58b36bf25
jakaria98 <jhossain439@gmail.com>
2021-07-10 23:08:35 +0600

    navbar partially done

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
-
+import logo from "../Travelers Hut.png";
+import { Link } from "react-router-dom";
 class Navbar extends Component {
   render() {
     return (
-        <nav className='navbar'>
-            <div className="nav-center"></div>
-        </nav>
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src={logo} alt="Travelers HUt" />
+            </Link>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
