3b40bd44900abacd090ce8cb0fee654260c9a793
jakaria98 <jhossain439@gmail.com>
2021-07-10 22:59:23 +0600

    logo imported. in css nav css added. navbar created

---
+++ client/src/component/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Navbar extends Component {
+  render() {
+    return (
+        <nav className='navbar'>
+            <div className="nav-center"></div>
+        </nav>
+    );
+  }
+}
+export default Navbar;
