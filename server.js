7ad751ee410440b74d967d340402d63e64aa3797
jakaria98 <jhossain439@gmail.com>
2021-08-05 02:00:57 +0600

    body-parser uninstalled

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
 
 const app = express();
 app.use(morgan("dev"));
 app.use(cors());
-app.use(bodyParser.urlencoded({ limit: "1024mb", extended: true }));
-app.use(bodyParser.json({ limit: "1024mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "1024mb", extended: true }));
+// app.use(bodyParser.json({ limit: "1024mb", extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(passport.initialize());
 require("./passport")(passport);
 
