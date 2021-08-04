f4ccda4bc05290500bb260bd4252671999725fde
jakaria98 <jhossain439@gmail.com>
2021-08-05 02:13:50 +0600

    created and all router added

--- routers/reportedPlaceRouter.js
+++ routers/reportedPlaceRouter.js
@@ -1,2 +1,12 @@
+const {
+  getAllReport,
+  getSingleReport,
+  deleteReport,
+} = require("../controller/reportedPlaceController");
+
 const router = require("express").Router();
+
+router.get("/", getAllReport);
+router.get("/:reportID", getSingleReport);
+router.delete("/:reportID", deleteReport);
 module.exports = router;
