9d6045ff40a76a1556ec39666c51e50924671a22
jakaria98 <jhossain439@gmail.com>
2021-08-26 16:07:17 +0600

    Guide register page design done

--- client/src/pages/Guide/Register.js
+++ client/src/pages/Guide/Register.js
@@ -4,7 +4,11 @@ import { connect } from "react-redux";
 import { register, registerRequest } from "../../store/actions/guideAction";
 import Modal from "react-modal";
 import { BiUserPin } from "react-icons/bi";
-import { MdEmail } from "react-icons/md";
+import { MdAddAPhoto, MdEmail } from "react-icons/md";
+import { FiPhoneCall } from "react-icons/fi";
+import { GrContact } from "react-icons/gr";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { GiCheckMark } from "react-icons/gi";
 
 const customStyles = {
   content: {
@@ -147,7 +151,7 @@ class Register extends React.Component {
                   </div>
                 </div>
               </div>
-              <div className="form-group my-3">
+              <div className="form-group my-2">
                 <div className="d-flex">
                   <MdEmail size={70} className="pt-4" />
                   <div className="container mx-1">
@@ -171,103 +175,127 @@ class Register extends React.Component {
                   </div>
                 </div>
               </div>
-
               <div className="form-group">
-                <label htmlFor="mobileNumber">Mobile Number: </label>
-                <input
-                  type="text"
-                  placeholder="Enter Your 11 Digit Mobile Number"
-                  name="mobileNumber"
-                  className={
-                    error?.mobileNumber
-                      ? "form-control is-invalid"
-                      : "form-control"
-                  }
-                  id="mobileNumber"
-                  value={mobileNumber}
-                  onChange={this.changeHandler}
-                />
-                {error?.mobileNumber && (
-                  <div className="invalid-feedback">{error?.mobileNumber}</div>
-                )}
-              </div>
-              <div className="form-group my-3">
-                <label htmlFor="profile photo">Profile Photo: </label>
-
-                <input
-                  type="file"
-                  name="profilePhoto"
-                  className={
-                    error?.profilePhoto
-                      ? "form-control is-invalid"
-                      : "form-control"
-                  }
-                  onChange={this.getPhoto}
-                />
-                {error?.profilePhoto && (
-                  <div className="invalid-feedback">{error?.profilePhoto}</div>
-                )}
-              </div>
-              <div className="form-group">
-                <label htmlFor="nid">NID: </label>
-                <input
-                  type="file"
-                  name="nid"
-                  className={
-                    error?.nid ? "form-control is-invalid" : "form-control"
-                  }
-                  multiple
-                  onChange={this.getPhoto}
-                />
-                {error?.nid && (
-                  <div className="invalid-feedback">{error?.nid}</div>
-                )}
-              </div>
-              <div className="form-group my-3">
-                <label htmlFor="password">Password: </label>
-                <input
-                  type="password"
-                  placeholder="Enter Your Password"
-                  name="password"
-                  className={
-                    error?.password ? "form-control is-invalid" : "form-control"
-                  }
-                  id="password"
-                  value={password}
-                  onChange={this.changeHandler}
-                />
-                {error?.password && (
-                  <div className="invalid-feedback">{error?.password}</div>
-                )}
-              </div>
-              <div className="form-group">
-                <label htmlFor="confirmPassword">Confirm Password: </label>
-                <input
-                  type="password"
-                  placeholder="Confirm Your Password"
-                  name="confirmPassword"
-                  className={
-                    error?.confirmPassword
-                      ? "form-control is-invalid"
-                      : "form-control"
-                  }
-                  id="confirmPassword"
-                  value={confirmPassword}
-                  onChange={this.changeHandler}
-                />
-                {error?.confirmPassword && (
-                  <div className="invalid-feedback">
-                    {error?.confirmPassword}
+                <div className="d-flex">
+                  <FiPhoneCall size={70} className="pt-4" />
+                  <div className="container mx-1">
+                    <label htmlFor="mobileNumber">Mobile Number: </label>
+                    <input
+                      type="text"
+                      placeholder="Enter Your 11 Digit Mobile Number"
+                      name="mobileNumber"
+                      className={
+                        error?.mobileNumber
+                          ? "form-control is-invalid"
+                          : "form-control"
+                      }
+                      id="mobileNumber"
+                      value={mobileNumber}
+                      onChange={this.changeHandler}
+                    />
+                    {error?.mobileNumber && (
+                      <div className="invalid-feedback">
+                        {error?.mobileNumber}
+                      </div>
+                    )}
                   </div>
-                )}
+                </div>
               </div>
-
-              <Link to="/guide/login" className="my-1">
-                Already Have An Account? Login Here.
-              </Link>
-
-              <button className="container btn btn-primary my-3 d-block center">
-                Apply
+              <div className="form-group my-2">
+                <div className="d-flex">
+                  <BiUserPin size={70} className="pt-4" />
+                  <div className="container mx-1">
+                    <label htmlFor="profile photo">Profile Photo: </label>
+                    <input
+                      type="file"
+                      name="profilePhoto"
+                      className={
+                        error?.profilePhoto
+                          ? "form-control is-invalid"
+                          : "form-control"
+                      }
+                      onChange={this.getPhoto}
+                    />
+                    {error?.profilePhoto && (
+                      <div className="invalid-feedback">
+                        {error?.profilePhoto}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="d-flex">
+                  <MdAddAPhoto size={70} className="pt-4" />
+                  <div className="container mx-1">
+                    <label htmlFor="nid">NID: </label>
+                    <input
+                      type="file"
+                      name="nid"
+                      className={
+                        error?.nid ? "form-control is-invalid" : "form-control"
+                      }
+                      multiple
+                      onChange={this.getPhoto}
+                    />
+                    {error?.nid && (
+                      <div className="invalid-feedback">{error?.nid}</div>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="form-group my-2">
+                <div className="d-flex">
+                  <RiLockPasswordFill size={70} className="pt-4" />
+                  <div className="container mx-1">
+                    <label htmlFor="password">Password: </label>
+                    <input
+                      type="password"
+                      placeholder="Enter Your Password"
+                      name="password"
+                      className={
+                        error?.password
+                          ? "form-control is-invalid"
+                          : "form-control"
+                      }
+                      id="password"
+                      value={password}
+                      onChange={this.changeHandler}
+                    />
+                    {error?.password && (
+                      <div className="invalid-feedback">{error?.password}</div>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="d-flex">
+                  <RiLockPasswordFill size={70} className="pt-4" />
+                  <div className="container mx-1">
+                    <label htmlFor="confirmPassword">Confirm Password: </label>
+                    <input
+                      type="password"
+                      placeholder="Confirm Your Password"
+                      name="confirmPassword"
+                      className={
+                        error?.confirmPassword
+                          ? "form-control is-invalid"
+                          : "form-control"
+                      }
+                      id="confirmPassword"
+                      value={confirmPassword}
+                      onChange={this.changeHandler}
+                    />
+                    {error?.confirmPassword && (
+                      <div className="invalid-feedback">
+                        {error?.confirmPassword}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+              <button className="container btn btn-success my-3 d-block center w-75">
+                Apply <GiCheckMark size={22} className="pb-1" />
               </button>
             </form>
           </div>
@@ -283,28 +311,33 @@ class Register extends React.Component {
                     <div className="col-md-6 offset-md-3">
                       <form onSubmit={this.codeSubmit}>
                         <div className="form-group">
-                          <label htmlFor="code">Code:</label>
-                          <input
-                            type="number"
-                            placeholder="Enter The Code From Message"
-                            name="code"
-                            className={
-                              error?.code
-                                ? "form-control is-invalid"
-                                : "form-control mb-3"
-                            }
-                            id="code"
-                            value={code}
-                            onChange={this.changeHandler}
-                          />
-                          {error?.code && (
-                            <div className="invalid-feedback mb-3">
-                              {error?.code}
+                          <div className="d-flex">
+                            <GrContact size={70} className="pt-4" />
+                            <div className="container mx-1">
+                              <label htmlFor="code">Code:</label>
+                              <input
+                                type="number"
+                                placeholder="Enter The Code From Message"
+                                name="code"
+                                className={
+                                  error?.code
+                                    ? "form-control is-invalid"
+                                    : "form-control mb-3"
+                                }
+                                id="code"
+                                value={code}
+                                onChange={this.changeHandler}
+                              />
+                              {error?.code && (
+                                <div className="invalid-feedback mb-3">
+                                  {error?.code}
+                                </div>
+                              )}
                             </div>
-                          )}
+                          </div>
                         </div>
-                        <button className="btn btn-primary container">
-                          Register
+                        <button className="btn btn-success container">
+                          Submit <GiCheckMark size={22} className="pb-1" />
                         </button>
                       </form>
                     </div>
