d58076b0a5935b3aec9d66f5792b637e0a13c739
jakaria98 <jhossain439@gmail.com>
2021-08-29 02:53:43 +0600

    navbar done

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -2,7 +2,10 @@ import React, { Component } from "react";
 import logo from "../Travelers Hut.png";
 import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaUsers, FaUserShield } from "react-icons/fa";
+import { GoLocation } from "react-icons/go";
+import { AiFillBug, AiOutlineLogin, AiOutlineLogout } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
 
 import { visitorLogout } from "../store/actions/visitorAction";
 import { guideLogout } from "../store/actions/guideAction";
@@ -16,6 +19,70 @@ class Navbar extends Component {
             <img src={logo} alt="Travelers Hut" />
           </Link>
         </div>
+        <label for="drop" className="check-btn">
+          <FaBars />
+        </label>
+        <input type="checkbox" id="drop" />
+        <ul className="menu">
+          <li>
+            <Link to="/all-places">
+              All Places
+              <GoLocation size={25} className="mx-1" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/admin/action/allGuide">
+              Guide List
+              <FaUsers size={25} className="mx-1" />
+            </Link>
+          </li>
+          <li>
+            <Link to="/all-admin">
+              Admin List
+              <FaUserShield size={25} className="mx-1" />
+            </Link>
+          </li>
+          <li>
+            <label for="drop-1" class="toggle">
+              Reports
+              <AiFillBug size={25} className="mx-1" />
+            </label>
+            <Link to="#">
+              Reports
+              <AiFillBug size={25} className="mx-1" />
+            </Link>
+            <input type="checkbox" id="drop-1" />
+            <ul>
+              <li>
+                <Link to="/all-places/reported-places">
+                  Places
+                  <AiFillBug size={25} className="mx-1" />
+                </Link>
+              </li>
+              <li>
+                <Link to="/all-places/reported-posts">
+                  Posts
+                  <AiFillBug size={25} className="mx-1" />
+                </Link>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <Link to="/my-profile">
+              My Profile
+              <CgProfile size={25} className="mx-1" />
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="#"
+              onClick={() => this.props.adminLogout(this.props.history)}
+            >
+              Logout
+              <AiOutlineLogout size={25} className="mx-1" />
+            </Link>
+          </li>
+        </ul>
       </nav>
     );
   }
