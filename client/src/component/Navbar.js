5bee7494153f145f02e24a17099948744aecda1a
jakaria98 <jhossain439@gmail.com>
2021-08-29 23:07:03 +0600

    navbar done

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import logo from "../Travelers Hut.png";
 import { Link, withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { FaBars, FaUsers, FaUserShield } from "react-icons/fa";
+import { FaBars, FaUserPlus, FaUsers, FaUserShield } from "react-icons/fa";
 import { GoLocation } from "react-icons/go";
 import { AiFillBug, AiOutlineLogin, AiOutlineLogout } from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
@@ -23,66 +23,142 @@ class Navbar extends Component {
           <FaBars />
         </label>
         <input type="checkbox" id="drop" />
-        <ul className="menu">
-          <li>
-            <Link to="/all-places">
-              All Places
-              <GoLocation size={25} className="mx-1" />
-            </Link>
-          </li>
-          <li>
-            <Link to="/admin/action/allGuide">
-              Guide List
-              <FaUsers size={25} className="mx-1" />
-            </Link>
-          </li>
-          <li>
-            <Link to="/all-admin">
-              Admin List
-              <FaUserShield size={25} className="mx-1" />
-            </Link>
-          </li>
-          <li>
-            <label for="drop-1" class="toggle">
-              Reports
-              <AiFillBug size={25} className="mx-1" />
-            </label>
-            <Link to="#">
-              Reports
-              <AiFillBug size={25} className="mx-1" />
-            </Link>
-            <input type="checkbox" id="drop-1" />
-            <ul>
-              <li>
-                <Link to="/all-places/reported-places">
-                  Places
-                  <AiFillBug size={25} className="mx-1" />
-                </Link>
-              </li>
-              <li>
-                <Link to="/all-places/reported-posts">
-                  Posts
-                  <AiFillBug size={25} className="mx-1" />
-                </Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to="/my-profile">
-              My Profile
-              <CgProfile size={25} className="mx-1" />
-            </Link>
-          </li>
-          <li>
-            <Link
-              to="#"
-              onClick={() => this.props.adminLogout(this.props.history)}
-            >
-              Logout
-              <AiOutlineLogout size={25} className="mx-1" />
-            </Link>
-          </li>
-        </ul>
+        {this.props.admin.isAuthenticated ? (
+          <ul className="menu">
+            <li>
+              <Link to="/all-places">
+                All Places
+                <GoLocation size={25} className="mx-1" />
+                <GoLocation size={25} className="mx-1" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/action/allGuide">
+                Guide List
+                <FaUsers size={25} className="mx-1" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/all-admin">
+                Admin List
+                <FaUserShield size={25} className="mx-1" />
+              </Link>
+            </li>
+            <li>
+              <label for="drop-1" class="toggle">
+                Reports
+                <AiFillBug size={25} className="mx-1" />
+              </label>
+              <Link to="#">
+                Reports
+                <AiFillBug size={25} className="mx-1" />
+              </Link>
+              <input type="checkbox" id="drop-1" />
+              <ul>
+                <li>
+                  <Link to="/all-places/reported-places">
+                    Places
+                    <AiFillBug size={25} className="mx-1" />
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/all-places/reported-posts">
+                    Posts
+                    <AiFillBug size={25} className="mx-1" />
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="/my-profile">
+                My Profile
+                <CgProfile size={25} className="mx-1" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="#"
+                onClick={() => this.props.adminLogout(this.props.history)}
+              >
+                Logout
+                <AiOutlineLogout size={25} className="mx-1" />
+              </Link>
+            </li>
+          </ul>
+        ) : this.props.guide.isAuthenticated ? (
+          <ul className="menu">
+            <li>
+              <Link to="/all-places">
+                All Places
+                <GoLocation size={25} className="mx-1" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/my-profile">My Profile</Link>
+            </li>
+            <li>
+              <Link
+                to="3"
+                onClick={() => this.props.guideLogout(this.props.history)}
+              >
+                Logout
+                <AiOutlineLogout size={25} className="mx-1" />
+              </Link>
+            </li>
+          </ul>
+        ) : this.props.visitor.isAuthenticated ? (
+          <ul className="menu">
+            <li>
+              <Link to="/all-places">
+                All Places
+                <GoLocation size={25} className="mx-1" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/guide/login">
+                Login as Guide
+                <AiOutlineLogin size={25} className="mx-1" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/guide/register">
+                Register as Guide
+                <FaUserPlus size={25} className="mx-1" />
+              </Link>
+            </li>
+            <li>
+              <Link to="/admin/login">
+                Login as Admin
+                <AiOutlineLogin size={25} className="mx-1" />
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="#"
+                onClick={() => this.props.visitorLogout(this.props.history)}
+              >
+                Logout
+                <AiOutlineLogout size={25} className="mx-1" />
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          <uL className="menu">
+            <li>
+              <Link to="/login">
+                Login
+                <AiOutlineLogin size={25} className="mx-1" />
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/register">
+                Register
+                <FaUserPlus size={25} className="mx-1" />
+              </Link>
+            </li>
+          </uL>
+        )}
       </nav>
     );
   }
@@ -117,7 +193,7 @@ export default connect(mapStateToProps, {
         !this.props.admin.isAuthenticated ? (
           <ul>
             <li>
-              <Link to="/all-places">All Places</Link>
+              <Link to="/all-places">All Places <GoLocation size={25} className="mx-1" /></Link>
             </li>
             <li>
               <Link to="/guide/login">Login as Guide</Link>
